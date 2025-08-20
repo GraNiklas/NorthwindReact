3f41bf9488637a8e5d712d2f5a39b14d4494024a
Niklas Granqvist <niklasgranqvist@windowslive.com>
2025-08-20 14:56:50 +0300

    search

--- src/CustomerList.jsx
+++ src/CustomerList.jsx
@@ -1,35 +1,51 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import customerService from './services/CustomerService';
 import Customer from './Customer';
 import CustomerAdd from './CustomerAdd';
 
 //props suoraan nimellä eikä props.huomio
-const CustomerList = ({setIsPositive,setMessage,setShowMessage}) => {
+const CustomerList = ({ setIsPositive, setMessage, setShowMessage }) => {
 
     //componentin tilan määritys
-    const [customers,setCustomers] = useState([]);
-    const [showCustomers,setShowCustomers] = useState(false);
-    const [lisäysTila,setLisäysTila] = useState(false);
-    
+    const [customers, setCustomers] = useState([]);
+    const [showCustomers, setShowCustomers] = useState(false);
+    const [lisäysTila, setLisäysTila] = useState(false);
+    const [search, setSearch] = useState("");
 
-    useEffect(()=>{
+    useEffect(() => {
         customerService.getAll()
-        .then(data => setCustomers(data))
-    },[lisäysTila])
+            .then(data => setCustomers(data))
+    }, [lisäysTila])
+
+
+    const handleSearch = (e) => {
+        setShowCustomers(true);
+        setSearch(e.target.value.toLowerCase());
+    }
 
     return (
-    <>
-        <button onClick={()=>setShowCustomers(!showCustomers)}>{showCustomers?"Piilota asiakkaat":"Näytä asiakkaat"}</button>
-        {!lisäysTila && <button className='button' onClick={()=>setLisäysTila(true)}>Add new</button>}
-        
-        {lisäysTila && <CustomerAdd setLisäysTila={setLisäysTila} setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage}/>}
-        {
-            showCustomers && customers && customers.map(c => 
-                <Customer key={c.customerId}  customer = {c} setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage} />
-            )
-        }
-    </>
-    
+        <>
+          
+
+            <button onClick={() => setShowCustomers(!showCustomers)}>{showCustomers ? "Piilota asiakkaat" : "Näytä asiakkaat"}</button>
+            {!lisäysTila && <button className='button' onClick={() => setLisäysTila(true)}>Lisää uusi</button>}
+            <br/>
+            <label>Search:</label>
+            <input type='text' onChange={(value) => handleSearch(value)}></input>
+            {lisäysTila && <CustomerAdd setLisäysTila={setLisäysTila} setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage} />}
+            {
+                showCustomers && customers && customers.map(c => {
+                    const lowerCaseName = c.companyName.toLowerCase();
+                    if (lowerCaseName.indexOf(search) > -1)
+                        return (
+                            <Customer key={c.customerId} customer={c} setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage} />
+                        )
+                }
+                )
+            }
+              
+        </>
+
     );
 }
 
